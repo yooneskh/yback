b55c495eb2695981bae6798a9eb9711e651a2799
Yoones Khoshghadam <yooneskh@gmail.com>
2021-11-21 01:25:51 +0330

    feat (bootstrap): modular bootstrap, create user script

--- bootstrap.ts
+++ bootstrap.ts
@@ -1,35 +1,16 @@
-import { Config }  from './config.ts';
+import { Config } from './config.ts';
 
 const timeStart = Date.now();
 
 
 async function bootstrapDatabase() {
-
-  const { connect } = await import('./deps.ts');
-
-  if (!( Config.database.connectionString || (Config.database.host && Config.database.port && Config.database.name) )) {
-    throw new Error('database connection information is insufficient');
-  }
-
-  const connectionString = Config.database.connectionString || `mongo://${Config.database.host}:${Config.database.port}/${Config.database.name}`;
-  await connect(connectionString);
-
+  const { bootstrap } = await import('./bootstrappers/bootstrap-database.ts');
+  await bootstrap();
 }
 
 async function bootstrapHttp() {
-
-  const { setupHttpTransport } = await import('./transports/http.ts');
-
-  if (!Config.http.port) {
-    throw new Error('http listen port not provided');
-  }
-
-  const httpListenAddress = `:${Config.http.port}`;
-
-  setupHttpTransport(Config.http.port, () => {
-    console.log(`http service listening on ${httpListenAddress} -- time took: ${Date.now() - timeStart} ms`);
-  });
-
+  const { bootstrap } = await import('./bootstrappers/bootstrap-http.ts');
+  await bootstrap(timeStart);
 }
 
 
