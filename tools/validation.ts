e63acea9abc72dfc1df9d8a3f6ac8d4afd806b6e
Yoones Khoshghadam <yooneskh@gmail.com>
2021-11-22 15:47:28 +0330

    feat (validation): add is slug validation

--- tools/validation.ts
+++ tools/validation.ts
@@ -4,3 +4,10 @@ const phoneNumberRegex = /^\+98[0-9]{10}$/;
 export function isPhoneNumber(text: unknown) {
   return typeof text === 'string' && phoneNumberRegex.test(text);
 }
+
+
+const slugRegex = /[a-z0-9\-]+/;
+
+export function isSlug(text: unknown) {
+  return typeof text === 'string' && slugRegex.test(text);
+}
