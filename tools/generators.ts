8a883574aaefbadd5a38d5a19fa58c95f326c9f0
Yoones Khoshghadam <yooneskh@gmail.com>
2021-12-07 00:51:01 +0330

    fix (generators): uuid fill

--- tools/generators.ts
+++ tools/generators.ts
@@ -1,6 +1,6 @@
 
 export function generateUUID(parts = 3) {
-  return (Array(parts).map(() => Math.random())
+  return (Array(parts).fill('').map(() => Math.random())
     .map(it => it.toString(22))
     .map(it => it.slice(2))
     .join('-')
