dd1dd68a5efb766f442718f9b5bbdd98e8ad367d
Yoones Khoshghadam <yooneskh@gmail.com>
2021-12-24 19:27:06 +0330

    fix (authorization): default fix

--- config.ts
+++ config.ts
@@ -26,7 +26,7 @@ export const Config = {
     tokenLifetime: 1000 * 60 * 60 * 24 * 30
   },
   authorization: {
-    defaultPermissions: ['user.*']
+    defaultPermissions: ['user.**']
   },
   payment: {
     core: {
